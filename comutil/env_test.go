922cf846d3070d5713cac8073024eb19f5829143
zhangxiaofeng05 <zhangxiaofeng05@foxmail.com>
2023-01-05 00:03:36 +0800

    fix: getEnv test

--- comutil/env_test.go
+++ comutil/env_test.go
@@ -2,18 +2,31 @@ package comutil_test
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
-	"github.com/zhangxiaofeng05/com/comutil"
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/zhangxiaofeng05/com/comutil"
 )
 
 func TestGetEnv(t *testing.T) {
+	k := "Jack"
+	v := "JackSstValue"
+	err := os.Setenv(k, v)
+	if err != nil {
+		t.Fatal("set env fail")
+	}
+	gv := os.Getenv(k)
+	if gv != v {
+		t.Fatal("get env fail")
+	}
 	list := []struct {
 		key   string
 		exist bool
 	}{
-		{"GOPATH", true},
-		{"GOPATH-NOT-EXIST", false},
+		{k, true},
+		{"JackNotExist", false},
 	}
 	for i, s := range list {
 		name := fmt.Sprintf("case %d", i)
