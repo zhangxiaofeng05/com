f4843e73951e8031126adf104e7ffdee16336fd6
zhangxiaofeng05 <zhangxiaofeng05@foxmail.com>
2023-01-19 18:47:24 +0800

    add comyaml test

--- comconfig/comyaml/yaml_test.go
+++ comconfig/comyaml/yaml_test.go
@@ -7,20 +7,56 @@ import (
 )
 
 func TestParseConfig(t *testing.T) {
-	type Config struct {
-		A string `yaml:"a" validate:"required"`
-		B struct {
-			RenamedC int   `yaml:"c" validate:"required"`
-			D        []int `yaml:",flow" validate:"required"`
-		} `yaml:"b" validate:"required"`
-	}
+	t.Run("pass", func(t *testing.T) {
+		type Config struct {
+			A string `yaml:"a" validate:"required"`
+			B struct {
+				RenamedC int   `yaml:"c" validate:"required"`
+				D        []int `yaml:",flow" validate:"required"`
+			} `yaml:"b" validate:"required"`
+		}
 
-	path := "testdata/test.yaml"
-	config := Config{}
+		path := "testdata/test.yaml"
+		config := Config{}
 
-	err := comyaml.Parse(path, &config)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("s: %+v", config)
+		err := comyaml.Parse(path, &config)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("s: %+v", config)
+	})
+
+	//file not exist
+	t.Run("file not exist", func(t *testing.T) {
+		path := "testdata/test_not_exist.yaml"
+		config := struct{}{}
+		err := comyaml.Parse(path, &config)
+		if err == nil {
+			t.Fatal("want err")
+		}
+	})
+
+	t.Run("file content not right", func(t *testing.T) {
+		path := "testdata/test1.yaml"
+		config := struct {
+			Name string `yaml:"name"`
+		}{}
+		err := comyaml.Parse(path, &config)
+		if err == nil {
+			t.Fatal("want err")
+		}
+	})
+
+	t.Run("file not pass validate", func(t *testing.T) {
+		path := "testdata/test2.yaml"
+		config := struct {
+			Name string `yaml:"name" validate:"required"`
+			Age  string `yaml:"age" validate:"required"`
+		}{}
+		err := comyaml.Parse(path, &config)
+		if err == nil {
+			t.Fatal("want err")
+		}
+	})
+
 }
