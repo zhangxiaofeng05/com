f5537f6869f0ac679bb7eeb471855377cdfaa2b9
zhangxiaofeng05 <zhangxiaofeng05@foxmail.com>
2024-06-25 16:01:43 +0800

    feat(com_heartbeat): init

---
+++ com_heartbeat/heartbeat.go
@@ -0,0 +1,18 @@
+package com_heartbeat
+
+import (
+	"net/http"
+	"strings"
+)
+
+// Heartbeat reference: https://github.com/go-chi/chi/blob/master/middleware/heartbeat.go
+func Heartbeat(endpoint string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if (r.Method == "GET" || r.Method == "HEAD") && strings.EqualFold(r.URL.Path, endpoint) {
+			w.Header().Set("Content-Type", "text/plain")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("pong"))
+			return
+		}
+	}
+}
